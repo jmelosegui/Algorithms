226ca249ca3f3294f7a7da4555607a5774ffc731
jmelosegui <elosegui@gmail.com>
2016-06-04 22:10:52 -0400

    Adding BinarySearch

---
+++ Algorithms.Test/BinarySearchFacts.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms.Test
+{
+    using Xunit;
+
+    public class BinarySearchFacts
+    {
+        [Fact]
+        public void Search()
+        {
+            // Arrange
+            var collection = new[] { 1, 2, 3, 4, 5 };
+
+            // Act
+            int index5 = BinarySearch.Search(collection, 6);
+            int index4 = BinarySearch.Search(collection, 5);
+            int index3 = BinarySearch.Search(collection, 4);
+            int index2 = BinarySearch.Search(collection, 3);
+            int index1 = BinarySearch.Search(collection, 2);
+            int index0 = BinarySearch.Search(collection, 1);
+            int indexNull7 = BinarySearch.Search(collection, 7);
+            int indexNull0 = BinarySearch.Search(collection, 0);
+
+            // Assert
+            Assert.Equal(5, index5);
+            Assert.Equal(4, index4);
+            Assert.Equal(3, index3);
+            Assert.Equal(2, index2);
+            Assert.Equal(1, index1);
+            Assert.Equal(0, index0);
+            Assert.Equal(-1, indexNull7);
+            Assert.Equal(-1, indexNull0);
+        }
+    }
+}
---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) to jmelosegui. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace Algorithms
+{
+    using System.Collections.Generic;
+
+    public static class BinarySearch
+    {
+        public static int Search<T>(IList<T> list, T itemToSearch)
+        {
+            return Search<T>(list, itemToSearch, 0, list.Count - 1);
+        }
+
+        private static int Search<T>(IList<T> list, T itemToSearch, int low, int high)
+        {
+            if (low > high)
+            {
+                return -1;
+            }
+
+            int middle = (high + low) / 2;
+
+            if (list[middle].GetHashCode() == itemToSearch.GetHashCode())
+            {
+                return middle;
+            }
+
+            if (list[middle].GetHashCode() > itemToSearch.GetHashCode())
+            {
+                return Search(list, itemToSearch, low, middle - 1);
+            }
+
+            return Search(list, itemToSearch, middle + 1, high);
+        }
+    }
+}
